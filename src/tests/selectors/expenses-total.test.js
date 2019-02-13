d26750d092c8f600e91e12c546318ac042339f16
Fabio Arazaki <fabioarazaki@gmail.com>
2019-02-13 13:04:28 -0200

    saved test and snapshots expense-total

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -1,9 +1,6 @@
 import selectExpensesTotal from '../../selectors/expenses-total.js';
 import expenses from '../fixtures/expenses';
 
-const total = selectExpensesTotal(expenses);
-console.log(total); // 114195
-
 test('should correctly add up multiple expenses', () => {
     const result = selectExpensesTotal(expenses);
     expect(result).toBe(114195);
